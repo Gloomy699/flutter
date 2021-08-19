d1139229ec92cd02fcd383990b8705c5670fc7e0
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-08-19 14:12:51 +0300

    upd

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/flutterTutorialApp.dart';
 
 void main() => runApp(FlutterTutorialApp());
